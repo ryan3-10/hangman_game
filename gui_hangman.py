ce42f4526743c68a3df5d71898244a8e2847cb2b
Ryan <ryan.coutts98@gmail.com>
2023-01-05 20:21:22 -0700

    Make game end
    Made the game come to an end once '_' was no longer in board list or lives reached 0.
    Created a "New Game" button for when the end results are displayed.

--- gui_hangman.py
+++ gui_hangman.py
@@ -6,13 +6,15 @@ win.title("Hangman Game")
 win.geometry("800x500")
 count = 0
 stand = 'Helvetica 13'
-lives = 6
+lives = 0
 
 def clear_window():
     for widget in win.winfo_children():
                 widget.destroy()
 
-def get_word():    
+def get_word():
+    global lives
+    lives = 6    
     clear_window()
     ttk.Label(win, text="Hanger, what is your word?", font=stand).pack()
     entry = Entry(win, width = 42)
@@ -29,11 +31,14 @@ def check_word(word, confirmed):
     count += 1
     check_match = Label(win, text="", font=stand)
     check_match.pack()
-    if word != confirmed:
+    if word != confirmed or len(word) < 2 or not word.isalpha(): 
         if count > 1:
             pass
         else:
-            check_match.config(text="Words do not match. Please try again.")
+            check_match.config(text="ERROR. Please make sure the following are true:")
+            ttk.Label(text="The words match\nOnly alphabetical characters are used\n"
+                            "The word is at least 2 characters long", font=stand).pack()
+                                    
     else:
         clear_window()
         play(word)
@@ -58,20 +63,26 @@ def check_guess(letter, string, list, label1, label2):
         list[string.index(letter)] = letter
         label1.config(text=list)
         if "_" not in list:
-            print("Guesser wins!")
+            clear_window()
+            end_game("Guesser", string)
     else:
         lives -= 1
         label2.config(text=str(lives) + " lives left")
         if lives == 0:
-            print("Hanger wins!")
+            clear_window()
+            end_game("Hanger", string)
 
-        
+
+def end_game(winner, word):
+    global lives
+    ttk.Label(win, text=word, font='Helvetica 32').pack()
+    ttk.Label(win, text=f"{lives} lives left. {winner} wins!").pack()
+    ttk.Button(win, text="New Game", command=get_word).pack()
+                        
 
 
 def main():
     get_word()
     win.mainloop()
-#main()
-play('max')
-win.mainloop()
+main()
 
